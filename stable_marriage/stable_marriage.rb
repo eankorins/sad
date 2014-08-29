a950e86ee38d5ee69c5e04f36221796c4631a399
Edgars Ankorins <eankorins@gmail.com>
2014-08-29 12:28:59 +0200

    Mutli threaded not working

--- stable_marriage/stable_marriage.rb
+++ stable_marriage/stable_marriage.rb
@@ -10,25 +10,54 @@ def read_file(name)
 	lines.slice!(lines.index("\n"))
   lines.slice!(0)
 	length = lines.count
-
+  puts 'Reading preferences'
   # Gets preference lists
-	preferences = lines.last(length / 2).map do |p|
+  chunks = lines.last(length / 2).each_slice(100)
+	threads = []
+  preferences = []
+  chunks.each do |chunk|
+    t = Thread.new { preferences << chunk.map do |p|
     p[3..p.length].split(' ').map(&:to_i)
+    end }
+    threads << t
+    puts threads.count
+    if threads.count == 20
+      threads.each { |thr| thr.join}
+      threads = []
+    end
   end 
 
+  threads.each{|t|t.join}
+  threads = []
+  prefs = []
+  preferences.each do |pref|
+    pref.each do |item|
+      prefs << item
+    end
+  end
+  puts "#{prefs}"
+  puts 'Creating people'
   # Map people as Person objects
+  p_index = 0
   people = lines.take(length / 2).each_with_index.map do |p, i|
     number, name = p.tr("\n", "").split(' ')
-    person = Person.new(name, number.to_i, preferences[i])
+    person = Person.new(name, number.to_i, prefs[i])
   end
-
   people.each(&:display)
-
+  puts 'Remapping preferences'
   # Remap every person's preference list to be an Person object reference
-  people.each do |person|
-    new_pref_list = person.preference_list.select{|pref| pref != 0 }.map{ |p| people.find{ |pref| pref.number == p}}
-    person.preference_list = new_pref_list
+  peoples = people.each_slice(100)
+  peoples.each do |people|
+    t = Thread.new { people.each do |person|
+      new_pref_list = person.preference_list.select{|pref| pref != 0 }.map{ |p| peoples.find{ |pref| pref.number == p}}
+      person.preference_list = new_pref_list end }
+      threads << t
+      if threads.count == 20
+        threads.each { |thr| thr.join}
+        threads = []
+      end
   end
+  threads.each{|t|t.join}
 end
 
 def match_couples(men, women)
@@ -43,14 +72,15 @@ puts File.dirname(__FILE__)
 directory_files(ARGV[0])
 
 people = read_file(ARGV[0])
-
+puts 'Getting men'
 men = people.select{ |p| p.number.odd? }
+puts 'Getting women'
 women = people.select{ |p| p.number.even? }
 
 
-men.each(&:display)
-women.each(&:display)
-
+# men.each(&:display)
+# women.each(&:display)
+'Matching'
 match_couples(men, women)
 
 men.each do |m|
