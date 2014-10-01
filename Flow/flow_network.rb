4125423ba3bbaf198d973f0a6b5b500fcdbaa4d3
Edgars Ankorins <eankorins@gmail.com>
2014-10-01 15:02:50 +0200

    Stuff

--- Flow/flow_network.rb
+++ Flow/flow_network.rb
@@ -1,120 +1,75 @@
-class Arc
-	attr_accessor :src, :dest, :capacity
+class Edge
+  attr_accessor :src, :dest, :capacity, :reverse_edge
 
-	def initialize(src, dest, capacity)
-		@src = src
-		@dest = dest
-		@capacity = capacity
-	end
+  def initialize(src, dest, capacity)
+  	@src = src 
+  	@dest = dest
+  	@capacity = capacity
+  end
+
+  def to_s
+  	"#{@src} -> #{dest}: #{capacity}"
+  end
 end
 
 class FlowNetwork
-	attr_accessor :source, :sink, :network
-
-
-	def initialize(source, sink, arcs)
-		@source = source
-		@sink = sink
-		@network = {}
-		arcs.each do |arc|
-			@network[arc] = 0
-		end
-	end
-
-	def ford_fulkerson
-		path = augmenting_path
-
-		while path 
-			flow_augmentation(path)
-			path = augmenting_path
-		end
-	end
-
-	def augmenting_path
-		labeled = { @source => nil }
-		scanned = {}
-
-		now_scanning = @source
-
-		while not labeled.empty?
-			if labeled.include?(@sink)
-				backtrace = [@sink]
-				parent = labeled[@sink]
-				while parent != nil
-					backtrace.push(parent)
-					parent = scanned[parent]
-				end
-				return backtrace.reverse!
-			end
-
-			arcs = @network.select do |k,v|
-				k.dest == now_scanning or k.src = now_scanning
-			end
-
-			arcs.each do |k,v|
-
-				if k.dest == now_scanning
-					if @network[k] < k.capacity
-						labeled[k.src] = now_scanning
-					end if not labeled.merge(scanned).include?(k.src)
-				elsif k.src == now_scanning
-					if @network[k] > 0 
-						labeled[e.dest] = now_scanning
-					end if not labeled.merge(scanned).include?(k.dest)
-				end
-			end
-			scanned[now_scanning] = labeled[now_scanning]
-			labeled.delete(now_scanning)
-			now_scanning = labeled.keys[0]
-		end	
-		return nil
-	end
-
-	def query_arc(src, dest)
-		@network.select do |k,v|
-			k.src == src and k.dest == dest
-		end
-	end
-
-	def flow_augmentation(path)
-		flow = +1.0/0 #Infinity and BEYOND
-
-		arcs = []
-
-		
-
-		path[0..path.length - 2].each_index do |i|
-			forward = query_edge(path[i], path[i+1])
-			backward = query_edge(path[i+1], path[i])
-
-			if backward.empty?
-				edge_flow = forward
-				edge.push(edge_flow.keys[0])
-				available = edge_flow.keys[0].capacity - edge_flow.values[0]
-				if flow > available
-					flow = available
-				end
-			elsif foward.empty?
-				edge_flow = backward
-				edge.push(edge_flow.keys[0])
-				available = edge_flow.values[0]
-				if flow > available
-					flow = available
-				end
-			end
-		end
-		arcs.each do |arc|
-			network[arc] += flow
-		end
-	end
-
-	def report
-    	print "Source: ", @source, "\n"
-    	print "Sink: ", @sink, "\n"
-    	@network.each_pair do |e, v|
-    	  print e.dest, ' -> ', e.src, '; capacity: ', e.capacity, ', flow: ', v, "\n"
-    end
-    puts
+  attr_accessor :source, :sink, :adj, :flow
+  
+  def initialize()
+    @adj = {}
+    @flow = {}
   end
 
+  def add_vertex(vertex)
+  	@adj[vertex] = []
+  end
+
+  def get_edges(vertex)
+  	return @adj[vertex]
+  end
+  
+  def add_edge(src, dest, c = 0)
+  	if src == dest
+  		puts "#{src} == #{dest}"
+  	else
+	  	edge = Edge.new(src, dest, c)
+	  	reverse_edge = Edge.new(dest, src, 0)
+	  	edge.reverse_edge = reverse_edge
+	  	reverse_edge.reverse_edge = edge
+	  	@adj[src] << edge
+	  	@adj[dest] << reverse_edge
+	  	@flow[edge] = 0
+	  	@flow[reverse_edge] = 0
+  	end
+  end
+
+  def find_path(source, sink, path)
+
+		return path if source == sink
+  	get_edges(source).each do |edge|
+  		residual = edge.capacity - @flow[edge]
+	 		if residual > 0 and not path.include?(edge)
+  			result = find_path(edge.dest, sink, path + [edge])
+  			return result if result != nil
+  		end
+  	end
+  end
+
+  def max_flow(source, sink)
+  	path = find_path(source, sink, [])
+  	puts "#{path}"
+  	while path != nil
+  		residuals = path.map { |edge| edge.capacity - @flow[edge] }
+  		flow = residuals.min
+
+  		path.each do |edge|
+  			@flow[edge] += flow
+  			@flow[edge.reverse_edge] -= flow
+  		end
+  		path = find_path(source, sink, [])
+
+		end
+		return get_edges(source).map { |edge| @flow[edge] }.sum
+		
+  end
 end
--- Flow/runner.rb
+++ Flow/runner.rb
@@ -4,21 +4,41 @@ require_relative 'flow_network'
 
 def run_file(file)
 	lines = open(file).readlines.map! { |x| x.gsub("\n",'')}
-	puts lines
 	length = lines.slice!(0).to_i
-
 	nodes = lines.slice!(0, length)
-	
-	arcLength = lines.slice!(0).to_i
 
-	arcs = lines.slice!(1, arcLength -1).map do |arc|
-		a, b, capacity =  arc.split(' ').map!(&:to_i)
-		Arc.new(nodes[a],nodes[b],capacity)
+	edgesLength = lines.slice!(0).to_i
+
+	puts "Vertices #{nodes.count} Edges #{edgesLength}"
+	edges = lines.slice!(0, edgesLength).map do |edge|
+		a, b, capacity =  edge.split(' ').map!(&:to_i)
+		capacity = 1000000 if capacity == -1
+		[nodes[a], nodes[b], capacity]
 	end
-	puts "#{arcs}"
-	network = FlowNetwork.new(nodes.first, nodes.last, arcs)
-	network.ford_fulkerson
+	network = FlowNetwork.new
+	nodes.each do |v|
+		network.add_vertex(v)
+	end
 
-	network.report
+	edges.each do |a,b,c|
+		network.add_edge(a,b,c)
+	end
+	puts network.max_flow('ORIGINS', 'DESTINATIONS')
 end
+
+g = FlowNetwork.new()
+"sopqrt".split('').each do |v|
+  g.add_vertex(v)
+end
+# g.add_edge('s','o',3)
+# g.add_edge('s','p',3)
+# g.add_edge('o','p',2)
+# g.add_edge('o','q',3)
+# g.add_edge('p','r',2)
+# g.add_edge('r','t',3)
+# g.add_edge('q','r',4)
+# g.add_edge('q','t',2)
+# puts g.max_flow('s','t')
+
+
 run_file('rail.txt')
